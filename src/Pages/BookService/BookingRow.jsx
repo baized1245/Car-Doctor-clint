1a5d6fbe385199d32a500f637b068da8d85880a8
unknown <you@example.com>
2023-08-29 05:34:04 +0600

    70.8

--- src/Pages/BookService/BookingRow.jsx
+++ src/Pages/BookService/BookingRow.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 
-const BookingRow = ({booking}) => {
-   const {date, service, price, img} = booking;
+const BookingRow = ({ booking, handleDelete }) => {
+    const {_id, date, service, price, img } = booking;
 
     return (
         <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <td>
-          
-            <div className="avatar">
-              <div className="rounded w-24 h-24">
-               { img && <img src={img} alt="Avatar Tailwind CSS Component" />}
-              </div>
-            </div>
-        </td>
-        <td>
-         {service}
-        </td>
-        <td>{date}</td>
-        <td>${price}</td>
-        <th>
-          <button className="btn btn-ghost btn-xs">details</button>
-        </th>
-      </tr>
+            <th>
+                <button onClick={() =>handleDelete(_id)} className="btn btn-sm btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                </button>
+            </th>
+            <td>
+
+                <div className="avatar">
+                    <div className="rounded w-24 h-24">
+                        {img && <img src={img} alt="Avatar Tailwind CSS Component" />}
+                    </div>
+                </div>
+            </td>
+            <td>
+                {service}
+            </td>
+            <td>{date}</td>
+            <td>${price}</td>
+            <th>
+                <button className="btn btn-ghost btn-xs">details</button>
+            </th>
+        </tr>
     );
 };
 
--- src/Pages/Bookings/Bookings.jsx
+++ src/Pages/Bookings/Bookings.jsx
@@ -15,6 +15,24 @@ const Bookings = () => {
     }))
    }, [])
 
+   const handleDelete = id => {
+    const proceed = confirm("Are you sure you want to delete this")
+    if(proceed){
+       fetch(`http://localhost:5000/bookings/${id}`, {
+        method: 'DELETE'
+       })
+       .then(res => res.json())
+       .then(data => {
+        console.log(data);
+        if(data.deletedCount > 0){
+            alert('Deleted Successfully')
+            const remaining = bookings.filter(booking => booking._id !== id);
+           setBookings(remaining)
+        }
+       })
+    }
+}
+
 
     return (
         <div>
@@ -29,10 +47,11 @@ const Bookings = () => {
             <input type="checkbox" className="checkbox" />
           </label>
         </th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-        <th></th>
+        <th>Image</th>
+        <th>Service</th>
+        <th>Date</th>
+        <th>Price</th>
+        <th>Status</th>
       </tr>
     </thead>
     <tbody>
@@ -40,6 +59,7 @@ const Bookings = () => {
        bookings.map(booking => <BookingRow
        key={booking._id}
        booking={booking}
+       handleDelete={handleDelete}
        ></BookingRow>) 
      } 
     </tbody>
