3457890b24e1b66dd6d2f0afd97a7c89211bbd05
unknown <you@example.com>
2023-08-30 22:23:38 +0600

    71.3

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,11 +1,15 @@
 import React, { useContext } from 'react';
 import img from '../../assets/images/login/login.svg'
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../Provider/AuthProvider';
 
 const Login = () => {
   
   const {signIn} = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const from =location?.state?.from?.pathname || "/";
 
    const handleLogin = event => {
     event.preventDefault();
@@ -17,6 +21,7 @@ const Login = () => {
     .then(result => {
       const user = result.user;
       console.log(user);
+      navigate(from, {replace: true});
     })
     .catch(error => console.log(error))
     
@@ -43,7 +48,7 @@ const Login = () => {
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                <input type="password" name='password' placeholder="password" className="input input-bordered" />
                 <label className="label">
                   <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                 </label>
