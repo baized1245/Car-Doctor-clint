b6a1c7051ffc725674dd9f70fa600c7e9e113c8b
unknown <you@example.com>
2023-08-31 00:03:27 +0600

    71.4

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -8,7 +8,9 @@ const Navbar = () => {
   const {user, logOut} = useContext(AuthContext)
   const handleLogOut = () => {
      logOut()
-     .then(() => {})
+     .then(() => {
+      localStorage.removeItem('car-access-token')
+     })
      .catch( error => console.log(error))
   }
 
